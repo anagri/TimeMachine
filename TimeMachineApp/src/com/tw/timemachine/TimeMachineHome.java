2ff9903ea343c21472ba176e18c11fa289821ad6
Amiruddin Nagri <anagri@thoughtworks.com>
2010-12-10 14:21:53 +0530

    [Amir] added projects and test project scaffold

---
+++ TimeMachineApp/src/com/tw/timemachine/TimeMachineHome.java
@@ -0,0 +1,13 @@
+package com.tw.timemachine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TimeMachineHome extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
